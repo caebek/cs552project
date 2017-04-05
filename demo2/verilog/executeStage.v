8dd8aaced3f204aba6761797ac6bb63fc7717b4f
Pritesh Kalantri <kalantri.pritesh@gmail.com>
2017-04-04 22:40:29 -0500

    execute and memory stage signals

--- demo2/verilog/executeStage.v
+++ demo2/verilog/executeStage.v
@@ -1,20 +1,23 @@
-module executeStage(instr, nextPc, err, halt, sign, pcOffSel, regWrt, memWrt, 
-	memEn, jump, invA, invB, return, cin, memToReg, writeReg, aluSrc, 
-	regWrtDataSrc, brType, aluOp, reg1Data, reg2Data, clk, rst, jumpPc);
-	
-	inout [15:0] nextPc, instr;
-	// signals from writeback stage 
+module executeStage(instr, nextPc, instrOut, nextPcOut, err, halt, sign, pcOffSel, regWrt, memWrt, 
+					memEn, jump, invA, invB, return, cin, memToReg, writeReg, aluSrc,regWrtDataSrc,
+					brType, aluOp, reg1Data, reg2Data, reg1DataOut, reg2DataOut, clk, rst, jumpPc,
+					setVal, doBranch);
 	
 	input halt, sign, pcOffSel, regWrt, memWrt, memEn, jump, invA, invB,
-		return, cin, memToReg;
-
+		return, cin, memToReg, clk, rst;
 	input [2:0] aluSrc, regWrtDataSrc, brType, writeReg;
 	input [3:0] aluOp;
+	input [15:0] reg1Data, reg2Data;
+	input [15:0] nextPc, instr;
 
-	inout [15:0] reg1Data, reg2Data;
-
+	output err, doBranch;
 	output [15:0] jumpPc, aluOut;
+	output [15:0] nextPcOut, instrOut;
+	output [15:0] reg1DataOut, reg2DataOut, setVal;
 
+	wire [15:0] base, offset, a, b;
+	wire zero, neg, ofl;
+	reg [2:0] hasErr;
 
 
 	localparam NOBR = 3'h0;
@@ -23,15 +26,15 @@ module executeStage(instr, nextPc, err, halt, sign, pcOffSel, regWrt, memWrt,
 	localparam LTZ = 3'h3;
 	localparam GEQZ = 3'h4;
 
-	wire [15:0] base, offset, a, b;
-
-
-
-
+	assign nextPcOut = nextPc;
+	assign instrOut = instr;
+	assign reg1DataOut = reg1Data;
+	assign reg2DataOut = reg2Data;
+	assign err = |hasErr;
 
 
 	// Jump/Branch PC calculation
-	assign base = (return) ? reg1Data : pcIncr;
+	assign base = (return) ? reg1Data : nextPc;
 	assign offset = (pcOffSel) ? {{3'h5{instr[10]}}, instr[10:0]} : {{4'h8{instr[7]}}, instr[7:0]};
 	cla16Bit adder(.A(base), .B(offset), .Cin(1'h0), .S(jumpPc));
 
@@ -47,8 +50,8 @@ module executeStage(instr, nextPc, err, halt, sign, pcOffSel, regWrt, memWrt,
 			NOBR: doBranch = 0;
 			EQZ: doBranch = zero;
 			NEZ: doBranch = ~zero;
-			LTZ: doBranch = n;
-			GEQZ: doBranch = ~n | zero;
+			LTZ: doBranch = neg;
+			GEQZ: doBranch = ~neg | zero;
 			default: hasErr[0] = 1'h1;
 		endcase
 	end
@@ -86,13 +89,6 @@ module executeStage(instr, nextPc, err, halt, sign, pcOffSel, regWrt, memWrt,
 	assign a = reg1Data;
 
 	alu alua(.A(a), .B(b), .Cin(cin), .Op(aluOp), .invA(invA), .invB(invB), 
-		.sign(sign), .Out(aluOut), .Ofl(ofl), .Zero(zero), .N(n));
-
-
-
-
-
-
-
+		.sign(sign), .Out(aluOut), .Ofl(ofl), .Zero(zero), .N(neg));
 
 endmodule
--- demo2/verilog/memoryStage.v
+++ demo2/verilog/memoryStage.v
@@ -1,28 +1,39 @@
-module memoryStage(clk, rst, err, aluOut, memWrt, memEn, halt, reg2Data, reg1Data, nextPc, instr);
+module memoryStage(clk, rst, err, aluOut, setVal, memWrt, memEn, halt, reg2Data, reg1Data, nextPc, instr, 
+					regWrtIn, regWrtOut, regWrtSrcIn, regWrtSrcOut, memOut, regWriteData);
 
-	input clk, rst, err, halt, memEn, memWrt;
-	input [15:0] aluOut, reg1Data, reg2Data, nextPc, instr;
+	input clk, rst, halt, memEn, memWrt;
+	input [15:0] aluOut, reg1Data, reg2Data, nextPc, instr, setVal;
+	input [2:0] regWrtSrcIn;
+	input regWrtIn;
+
 	output [15:0] memOut, regWriteData;
-	inout regWrt;
-	inout [2:0] regWrtSrc;
+	output [2:0] regWrtSrcOut;
+	output regWrtOut;
+	output reg err;
 
-	memory2c mem(.data_out(memOut), .data_in(dataIn), .addr(wrtAddr), .enable(memEn), .createdump(err|halt), 
+	memory2c mem(.data_out(memOut), .data_in(reg2Data), .addr(aluOut), .enable(memEn), .createdump(err|halt), 
 		.wr(memWrt), .clk(clk), .rst(rst));
 
+	assign regWrtOut = regWrtIn;
+	assign regWrtSrcOut = regWrtSrcIn;
+
+//	dff mOut [15:0] (.d(memOut), .q(memOut), .clk(clk), .rst(rst));
 
 
 	always @(*) begin
-		hasErr[2] = 0;
+		err = 0;
 		writeData = 16'h0;
 		case(regWrtSrc)
 			3'h0: writeData = memOut;
 			3'h1: writeData = aluOut;
-			3'h2: writeData = pcIncr;
+			3'h2: writeData = nextPc;
 			3'h3: writeData = setVal; // probably update this name
 			3'h4: writeData = {{4'h8{instr[7]}},instr[7:0]};
 			3'h5: writeData = {reg1Data[7:0], instr[7:0]};
-			3'h6: writeData = {reg1Data[0], reg1Data[1], reg1Data[2], reg1Data[3], reg1Data[4], reg1Data[5], reg1Data[6], reg1Data[7], reg1Data[8], reg1Data[9], reg1Data[10], reg1Data[11], reg1Data[12], reg1Data[13], reg1Data[14], reg1Data[15]}; 
-			default: hasErr[2] = 1'h1;
+			3'h6: writeData = {	reg1Data[0], reg1Data[1], reg1Data[2], reg1Data[3], reg1Data[4], reg1Data[5], 
+								reg1Data[6], reg1Data[7], reg1Data[8], reg1Data[9], reg1Data[10], reg1Data[11],
+								reg1Data[12], reg1Data[13], reg1Data[14], reg1Data[15]}; 
+			default: err = 1'h1;
 		endcase
 	end
 
